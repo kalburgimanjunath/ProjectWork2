46acdf7359ca15d0a92251d56f19f98b000900a8
galibgaurav <galibgaurav@gmail.com>
2017-04-13 12:38:21 +0530

    added javascript

--- ProjectWork.Web/App_Start/BundleConfig.cs
+++ ProjectWork.Web/App_Start/BundleConfig.cs
@@ -11,15 +11,39 @@ namespace ProjectWork.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/ThirdParty/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.unobtrusive*",
                 "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
-                "~/Scripts/knockout-{version}.js",
-                "~/Scripts/knockout.validation.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+            //    "~/Scripts/knockout-{version}.js",
+            //    "~/Scripts/knockout.validation.js"));
+            bundles.Add(new ScriptBundle("~/bundles/ThirdParty").Include(
+                "~/Scripts/ThirdParty/jquery-{version}.js",
+                "~/Scripts/ThirdParty/bootstrap.js",
+                "~/Scripts/ThirdParty/toastr.js",
+                "~/Scripts/ThirdParty/jquery.raty.js",
+                "~/Scripts/ThirdParty/respond.js",
+                //"~/Scripts/ThirdParty/respond.src.js",
+                "~/Scripts/ThirdParty/angular.js",
+                "~/Scripts/ThirdParty/angular-route.js",
+                "~/Scripts/ThirdParty/angular-cookies.js",
+                "~/Scripts/ThirdParty/angular-validator.js",
+                "~/Scripts/ThirdParty/angular-base64.js",
+                "~/Scripts/ThirdParty/angular-file-upload.js",
+                "~/Scripts/ThirdParty/angucomplete-alt.min.js",
+                "~/Scripts/ThirdParty/ui-bootstrap-tpls.js",
+                "~/Scripts/ThirdParty/underscore.js",
+                "~/Scripts/ThirdParty/raphael.js",
+                "~/Scripts/ThirdParty/morris.js",
+                "~/Scripts/ThirdParty/jquery.fancybox.js",
+                "~/Scripts/ThirdParty/jquery.fancybox-media.js",
+                "~/Scripts/ThirdParty/loading-bar.js"));
+
+
+
 
             bundles.Add(new ScriptBundle("~/bundles/app").Include(
                 "~/Scripts/sammy-{version}.js",
